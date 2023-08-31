a3a08b3cfbe1df9c6a70adf0ef06b50ff13a3194
artemslt <ppt.artem@gmail.com>
2023-08-31 12:56:45 +0300

    asdasd

--- app/[lang]/(shop)/charity/page.jsx
+++ app/[lang]/(shop)/charity/page.jsx
@@ -5,33 +5,24 @@ import { useLang } from "@/hooks/useLang";
 import { useDictionary } from "@/hooks/useDictionary";
 import Loader from "../../components/GeneralComponents/Loader/Loader";
 import styles from "./charity.module.scss";
+import { en } from "@/dictionaries/en";
+// import en from "../../../../dictionaries/en";
 
-const CharityPage = () => {
-    const lang = useLang();
-    const dictionary = useDictionary(lang);
-
+const CharityPage = ({ params: { lang } }) => {
     return (
-        <>
-            {dictionary ? (
-                <section className="container section">
-                    <h2 className="title--secondary">
-                        {dictionary?.charityPage.charity}
-                    </h2>
-                    <div className={styles.wrapper__desktop}>
-                        <div className={styles.wrapper}>
-                            <div className={styles.charity__imgThumb}></div>
-                            <p className={`text ${styles.about__us} `}>
-                                {dictionary?.charityPage.charityText}
-                            </p>
-                        </div>
-                        <TextSection />
-                    </div>
-                    <BankDetailsSection />
-                </section>
-            ) : (
-                <Loader />
-            )}
-        </>
+        <section className="container section">
+            <h2 className="title--secondary">{en.charityPage.charity}</h2>
+            <div className={styles.wrapper__desktop}>
+                <div className={styles.wrapper}>
+                    <div className={styles.charity__imgThumb}></div>
+                    <p className={`text ${styles.about__us} `}>
+                        {en.charityPage.charityText}
+                    </p>
+                </div>
+                {/* <TextSection /> */}
+            </div>
+            {/* <BankDetailsSection /> */}
+        </section>
     );
 };
 
--- hooks/useLang.js
+++ hooks/useLang.js
@@ -2,7 +2,6 @@ import { useParams } from "next/navigation";
 
 export const useLang = () => {
     const params = useParams();
-    console.log("is :>> ", "is");
-    console.log("params.lang :>> ", params.lang);
+
     return params.lang;
 };
